a508834429945de9b8ec37ca79540ff58e397c8a
Honorable con <honorablecon@gmail.com>
2022-06-15 22:55:25 +0000

    Set graphql controller

--- src/main/java/dev/honorablecon/rh/organisationEffectif/BootStrap.java
+++ src/main/java/dev/honorablecon/rh/organisationEffectif/BootStrap.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrganisationEffectifApplication {
+public class BootStrap {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrganisationEffectifApplication.class, args);
+		SpringApplication.run(BootStrap.class, args);
 	}
 
 }
